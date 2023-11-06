6aa8343c3681f2ca317b0ac96c9a71615bc87c82
khanhvu1412 <dkv1412@gmail.com>
2023-11-06 19:11:23 +0700

    first commit

---
+++ view/home.php
@@ -0,0 +1,175 @@
+ <!-- Content -->
+ <div class="container-fluid">
+
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Trang chủ</h1>
+</div>
+
+<div class="row">
+
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <a href="#">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Quản lý tài khoản</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-user-circle fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </a>
+        </div>
+    </div>
+
+   
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <a href="#">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Quản lý danh mục</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-clipboard fa-2x text-gray-300"></i>
+                        
+                    </div>
+                </div>
+            </div>
+        </a>
+        </div>
+    </div>
+
+   
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <a href="#">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                            Quản lý sản phẩm</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-box-open fa-2x text-gray-300"></i>
+                        
+                    </div>
+                </div>
+            </div>
+        </a>
+        </div>
+    </div>
+
+    
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <a href="#">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                            Quản lý bình luận</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    </div>
+</div>
+</div>
+
+<div class="container-fluid">
+
+<div class="card shadow mb-4">
+<div class="card-header py-3">
+    <h4 class="m-0 font-weight-bold text-primary">Quản lý tài khoản</h4>
+</div>
+<div class="card-body">
+        
+<div class="table-responsive">
+        <h5><label for="">Tìm  kiếm</label></h5>
+        <div>
+            
+                <form class="d-none d-sm-inline-block form-inline navbar-search">
+                    <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small"  aria-describedby="basic-addon2"  required>
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+                </form>
+            </div>
+                <br>
+        <table class="table table-bordered text-center" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Passworh</th>
+                    <th>Họ tên</th>
+                    <th>Ảnh</th>
+                    <th>Số điện thoại</th>
+                    <th>Vai trò</th>
+                    <th>Hành động</th>
+                    
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td>ID</td>
+                    <td>Email</td>
+                    <td>Password</td>
+                    <td>Họ tên</td>
+                    <td>Ảnh</td>
+                    <td>Số điện thoại</td>
+                    <td>Vai trò</td>
+                    <td>Hành động</td>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>vudkph37645@fpt.edu.vn</td>
+                    <td>**********</td>
+                    <td>Đỗ Khánh Vũ</td>
+                    <td><img src="img/3.jpg" width="150"></td>
+                    <td>09123743818</td>
+                    <td>Admin</td>
+                    <td>
+                        <input type="button" class="btn btn-primary" value="Sửa">
+                        <input type="button" class="btn btn-primary" value="Xóa">
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>huygp@gmail.com</td>
+                    <td>**********</td>
+                    <td>Phạm Gia Huy</td>
+                    <td><img src="img/4.jpg" width="150"></td>
+                    <td>03278382748</td>
+                    <td>Nhân viên</td>
+                    <td>
+                        <input type="button" class="btn btn-primary" value="Sửa">
+                        <input type="button" class="btn btn-primary" value="Xóa">
+                            </td>
+                        </tr>
+                    </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</div>
+<!--End Content -->
+
+</div>
