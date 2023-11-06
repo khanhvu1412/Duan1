7e471d8f585a5adb29bf8da80d06582976fdb8e3
khanhvu1412 <dkv1412@gmail.com>
2023-11-07 00:15:19 +0700

    first commit

--- Admin/index.php
+++ Admin/index.php
@@ -8,22 +8,43 @@ if (isset($_GET['act'])) {
     $act = $_GET['act'];
     switch ($act) {
 
+        // Tài khoản
+        case "updatetk":
+            include("taikhoan/update.php");
+            break;
         case "listtk":
             include("taikhoan/list.php");
             break;
 
+        // Danh mục
         case "listdm":
             include("danhmuc/list.php");
             break;
+        case "adddm":
+            include("danhmuc/add.php");
+            break;
+        case "updatedm":
+            include("danhmuc/update.php");
+            break;
+        
 
+        // Sản phẩm
         case "listsp":
             include("sanpham/list.php");
             break;
+        case "addsp":
+            include("sanpham/add.php");
+            break;
+        case "updatesp":
+            include("sanpham/update.php");
+            break;
 
+        // Bình luận
         case "listbl":
             include("binhluan/list.php");
             break;
 
+        // Thống kê
         case "thongke":
             include("thongke/list.php");
             break;
