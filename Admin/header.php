80e146f13e6f284812b9487930168abad7adbc29
khanhvu1412 <dkv1412@gmail.com>
2023-11-08 19:48:44 +0700

    first commit

--- Admin/header.php
+++ Admin/header.php
@@ -101,27 +101,29 @@
                     </form>
 
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown no-arrow d-flex align-items-center">
-                            <div>
-                                <!-- <a href="dangky.html"> Đăng ký </a>/ -->
-                                Admin 
-                                <!-- <a href="dangnhap.php"> Đăng nhập </a> -->
 
-                            </div>
+                    
+                    <div class="topbar-divider d-none d-sm-block"></div>
+                        <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-
-                                </span>
-
-                                <div>
-                                    <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
-
-                                </div>
-
-
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Tài khoản
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
                         </li>
                     </ul>
+
+
                 </nav>
-                
