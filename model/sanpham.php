24533e1826dfcc955259a7944d2eac7d5cefe0c9
khanhvu1412 <dkv1412@gmail.com>
2023-11-15 11:14:45 +0700

    first commit

--- model/sanpham.php
+++ model/sanpham.php
@@ -38,9 +38,9 @@ function loadone_sanpham($id)
 function update_sanpham($id, $iddm, $tensp, $giasp, $mota, $hinh)
 {
     if ($hinh != "") {
-        $sql = "update sanham set iddm = '" . $iddm . "' , tensp = '" . $tensp . "', giasp = '" . $giasp . " ', mota = '" . $mota . "', img = '" . $hinh . "' where id = " . $id;
+        $sql = " update sanpham set iddm = '" . $iddm . "' , tensp = '" . $tensp . "', giasp = '" . $giasp . " ', mota = '" . $mota . "', img = '" . $hinh . "' where id = " . $id;
     } else {
-        $sql = "update sanham set iddm = '" . $iddm . "' , tensp = '" . $tensp . "', giasp = '" . $giasp . " ', mota = '" . $mota . "' where id = " . $id;
+        $sql = " update sanpham set iddm = '" . $iddm . "' , tensp = '" . $tensp . "', giasp = '" . $giasp . " ', mota = '" . $mota . "' where id = " . $id;
     }
     pdo_execute($sql);
 }
